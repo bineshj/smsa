001c0a7923a16bf2a5674cfbc8f4dfce24146641
bineshj <bineshj@users.noreply.github.com>
2014-08-19 16:37:54 +0530

    Update winrmi.bat

--- winrmi.bat
+++ winrmi.bat
@@ -1,5 +1,2 @@
-C:\Users\Administrator> winrm quickconfig -q
-C:\Users\Administrator> winrm set winrm/config/winrs @{MaxMemoryPerShellMB="300"}
-C:\Users\Administrator> winrm set winrm/config/service @{AllowUnencrypted="true"}
-C:\Users\Administrator> winrm set winrm/config/service/auth @{Basic="true"}
-C:\Users\Administrator> winrm set winrm/config @{MaxTimeoutms="1800000"} 
+cd C:\Users\Administrator
+winrm quickconfig -q
