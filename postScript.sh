0a80cafcde86699214d3f988625120b442b3869c
bineshj <bineshj@users.noreply.github.com>
2015-06-05 14:15:16 +0530

    Update postScript.sh

--- postScript.sh
+++ postScript.sh
@@ -1,2 +1,2 @@
 ip=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/')
-wget http://119.81.204.35:8082/config?ipaaddress=$ip
+wget http://119.81.204.35:8082/config?ipaddress=$ip
